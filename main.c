35c0f2d27c561f46b27ea8e1b2a4edbd2a75b5b9
lyao4321 <lyao3@buffalo.edu>
2024-02-07 11:59:10 -0500

    added some argument identifiers

--- main.c
+++ main.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
+#include <string.h>
 
-int main() {
-    printf("Hello, World!\n");
+int main(int argc, char *argv[]) {
+    char *filename = NULL;
+    int max_flag = 0, min_flag = 0, field_flag;
+
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "-f") == 0) {
+           field_flag = 1;
+           printf("Flag -f\n");
+        } else if (strcmp(argv[i], "-max") == 0) {
+            max_flag = 1;
+            printf("Flag -max\n");
+        } else if (strcmp(argv[i], "-min") == 0) {
+            min_flag = 1;
+            printf("Flag -min\n");
+        } else {
+            printf("Unknown argument: %s\n", argv[i]);
+        }
+    }
+
+
     return 0;
 }
